c5538b0ddd088eaa92b2fd8612feae52d2909f43
rhenoresky <rheno2002@gmail.com>
2023-05-09 15:55:40 +0700

    first commit

---
+++ biodata.js
@@ -0,0 +1,6 @@
+let nama = 'Rheno Resky Nugraha'
+let tempatLahir = 'Cianjur'
+let tanggalLahir = '19 Desember 2002'
+let alamat = 'Kp Mekarmulya, RT01/RW05, Desa Sukaratu, Kec. Bojongpicung'
+
+console.log(`Nama: ${nama}\nTempat Lahir: ${tempatLahir}\nTanggal Lahir: ${tanggalLahir}\nAlamat: ${alamat}`)
